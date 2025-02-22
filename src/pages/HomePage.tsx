7640d2b938c3fdeced21233684f84f9b5244d54a
arel latoga <arel.latoga@hotmail.com>
2025-02-22 09:32:52 +0000

    add fluff in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,52 +1,57 @@
 export const HomePage = () => {
     return (
         <p>
-            Hi, I'm Arel.
-            <br/>
-            <br/>
-            I have a strong desire for creating technologies for social good.
-            <br />
-            Technologies that are beneficial to everyone.
-            <br/>
-            Really, technologies that aren't evil.
-            <br/>
-            And aren't known to pull rugs.
-            <br/>
-            Or steal information without consent.
-            <br/>
-            Or exacerbate an existing digital, physical or socio-economic divide.
-            <br/>
-            <br/>
+                Hi, I'm Arel.
+                <br/>
+                <br/>
+                I have a strong desire for creating technologies for social good.
+                <br/>
+                Technologies that are beneficial to everyone.
+                <br/>
+                Really, technologies that aren't evil.
+                <br/>
+                And aren't known to pull rugs.
+                <br/>
+                Or steal information without consent.
+                <br/>
+                Or exacerbate an existing digital, physical or socio-economic divide.
+                <br/>
+                <br/>
 
-            To this end, I am a programmer. A backend engineer.
-            I have years of experience in JavaScript, TypeScript, Java, and
-            Python.
+                To this end, I am a programmer. A backend engineer.
+                I have years of experience in JavaScript, TypeScript, Java, and
+                Python.
 
-            <br/>
-            <br/>
-            As a backend engineer, I test my own code.
-            I don't wait for the Quality Engineers to test my code for me.
-            <br/>
-            Although doing it together is also good.
+                <br/>
+                <br/>
+                As a backend engineer, I test my own code.
+                I don't wait for the Quality Engineers to test my code for me.
+                <br/>
+                Although doing it together is also good.
 
-            <br/>
-            <br/>
-            As a backend engineer, I deploy my own code.
-            Or rather, I let the CI/CD do it for me.
-            <br/>
-            Tinkering with the CI/CD is something I only want to do once.
+                <br/>
+                <br/>
+                As a backend engineer, I deploy my own code.
+                Or rather, I let the CI/CD do it for me.
+                <br/>
+                Tinkering with the CI/CD is something I only want to do once.
 
-            <br/>
-            <br/>
+                <br/>
+                <br/>
 
-            As a backend engineer, I also know a bit of frontend. lol
+                As a backend engineer, I also know a bit of frontend. lol
 
-            <br />
-            <br/>
-            So if you would like to work together,
-            feel free to reach out at
-            <p className="font-mono">arel(dot)latoga(at)hotmail(full-stop)com</p>
-            <br/>
+                <br/>
+                <br/>
+
+                And as a backend engineer, I always make sure to communicate. Two ways. In a dyadic manner. No strings attached. Kindness is free.
+
+                <br/>
+                <br/>
+                So if you would like to work together,
+                feel free to reach out at
+                <p className="font-mono">arel(dot)latoga(at)hotmail(full-stop)com</p>
+                <br/>
         </p>
     );
 };
